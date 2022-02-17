6da948e1007029a30553aa4113b1c9359d62d787
Christopher Oliver <cnoliver86@gmail.com>
2022-02-18 00:43:35 +0200

    build(scss): add theme css/sass packaging
    add stylesheet packaging (with sass support)

--- config/fileTransformer.js
+++ config/fileTransformer.js
@@ -1,9 +1,10 @@
 // source: https://jestjs.io/docs/code-transformation#examples
 
-const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
 
 module.exports = {
   process(src, filename, config, options) {
-    return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
+    return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';'
   },
-};
+}
