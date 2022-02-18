b35a43c0b4f53399e78dd83520ed6e034a83af20
Christopher Oliver <cnoliver86@gmail.com>
2022-02-18 09:33:43 +0200

    fix(lib): fix code smell

--- src/domain/interfaces/link-socket-options.interface.ts
+++ src/domain/interfaces/link-socket-options.interface.ts
@@ -1,5 +1,4 @@
 import { ICoordinates } from './coordinates.interface'
-import { ICircleStyle } from './circle-style.interface'
 import { ISocketOptions } from './socket-options.interface'
 
 export interface ILinkSocketOptions<T> extends ISocketOptions<T> {
